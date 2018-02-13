f4a732efe16624a1c780f7d20b9e8995f31e4ebf
David Lam <davidlys@gmail.com>
2018-02-13 15:54:51 +0800

    rename all item input ids and names. still broken

--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 
 from lists.models import Item, List
-from lists.forms import  ItemForm
+from lists.forms import  ItemForm, EMPTY_ITEM_ERROR
 
 class HomePageTest(TestCase):
 
@@ -55,6 +55,19 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/list/new', data={'text':''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/list/new', data={'text':''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text':''})
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 class NewItemTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -3,23 +3,19 @@ from lists.forms import ItemForm
 from lists.models import Item, List
 
 def home_page(request):
-    return render(request, 'home.html')
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'list': list_})
+    return render(request, 'home.html', {'form': ItemForm()})
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
 
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
-
-def home_page(request):
-    return render(request, 'home.html', {'form': ItemForm()})
