81eabfe0f4a308064de1caf56a6bb2428847b918
David Lam <davidlys@gmail.com>
2018-02-13 14:37:49 +0800

    rename all item input ids and names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,7 +11,7 @@ class ItemValidationTest(FunctionalTest):
         # on the empty input box
         self.browser.get(self.live_server_url)
 
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error saying
         # that list items cannot be blank
@@ -22,15 +22,15 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # She tries again with some text for the item, which now works
+        inputbox = self.get_item_input_box()
+        inputbox.send_keys('Buy milk')
 
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
 
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
 
@@ -41,7 +41,7 @@ class ItemValidationTest(FunctionalTest):
 
         # And she can correct it by filling some text in
 
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        inputbox.send_keys('Make tea')
+        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -46,13 +46,13 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
@@ -64,7 +64,7 @@ class NewItemTest(TestCase):
 
         self.client.post(
             f'/lists/{correct_list.id}/add_item',
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
         )
 
         self.assertEqual(Item.objects.count(), 1)
@@ -78,7 +78,7 @@ class NewItemTest(TestCase):
 
         response = self.client.post(
             f'/lists/{correct_list.id}/add_item',
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
